0f4c99333e1a0972afa1a0280ca210f03bc69b9f
Anuragini Sinha <ansi3987@colorado.edu>
2021-04-26 05:07:11 +0000

    complete
    BOOK implementation
    only!

--- mm.c
+++ mm.c
@@ -137,9 +137,17 @@ static void checkblock(void *bp);
 //
 int mm_init(void) 
 {
-  //
-  // You need to provide this
-  //
+  if((heap_listp = mem_sbrk(4*WSIZE)) == (void*)-1)
+      return -1;
+  PUT(heap_listp,0);
+  PUT(heap_listp,+ (1*WSIZE),PACK(DSIZE 1));
+  PUT(heap_listp,+ (2*WSIZE),PACK(DSIZE 1));
+  PUT(heap_listp,+ (3*WSIZE),PACK(0, 1));
+  heap_listp += (2*WSIZE);
+    
+  if (extend_heap(CHUNKSIZE/WSIZE)== NULL)
+  return -1;
+
   return 0;
 }
 
@@ -149,9 +157,11 @@ int mm_init(void)
 //
 static void *extend_heap(uint32_t words) 
 {
-  //
-  // You need to provide this
-  //
+  char *bp 
+  size_t size;
+  
+  size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
+  if((long)(bp = mem_sbrk(size)) == -1)
   return NULL;
 }
 
@@ -163,17 +173,30 @@ static void *extend_heap(uint32_t words)
 //
 static void *find_fit(uint32_t asize)
 {
-  return NULL; /* no fit */
-}
+    void *bp;
+    for(bp = heap_listp; GETSIZE(HRDP(bp)) > 0l bp = NEXT_BKLP(bp)) {
+        
+    }
+    
+        if(!GET_ALLOC(HRDP(bp)) && (asize <= GET_SIZE(HRDP(bp)))
+           {
+               return bp;
+           }
+        
 
+  
+}
+ return NULL; /* no fit */
 // 
 // mm_free - Free a block 
 //
 void mm_free(void *bp)
 {
-  //
-  // You need to provide this
-  //
+  size_t size = GET_SIZE(HDRP(bp));
+  PUT(HRDP(bp),PACK(size,0));
+  PUT(FTRP(bp),PACK(size,0));
+  coalesce(bp);
+    
 }
 
 //
@@ -181,6 +204,35 @@ void mm_free(void *bp)
 //
 static void *coalesce(void *bp) 
 {
+    size_t = prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+    size_t = next_alloc = GET_ALLOC(HRDP(NEXT_BLKP(bp)));
+    size_t size = GET_SIZE(HRDP(bp));
+    if(prev_alloc && next_alloc)
+    {
+        return bp;
+    }
+    else if(prev_alloc && !next_alloc) 
+    {
+       size+= GET_SIZE(HRDP(NEXT_BLKP(bp)));
+       PUT(HRDP(bp), PACK(size,0));
+       PUT(FTRP(bp),PACK(size,0));
+        
+    }
+    else if(!prev_alloc && next_alloc) 
+    {
+        size+= GET_SIZE(HRDP(PREV_BLKP(bp)));
+        PUT(FTRP(bp), PACK(size,0));
+        PUT(HRDP(PREV_BLKP(bp)), PACK(size,0));
+        bp = PREV_BLKP(bp);
+    }
+    else 
+    {
+       size+= GET_SIZE(HRDP(PREV_BLKP)(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
+       
+        PUT(HRDP(PREV_BLKP(bp)),PACK(size,0));
+        PUT(FTRP(NEXT_BLKP(bp)),PACK(size,0));
+    }
+    
   return bp;
 }
 
@@ -189,10 +241,30 @@ static void *coalesce(void *bp)
 //
 void *mm_malloc(uint32_t size) 
 {
-  //
-  // You need to provide this
-  //
-  return NULL;
+  size_t asize;
+  size_t extendsize;
+  char *bp;
+  
+    if(size == 0){
+        return NULL;
+    }
+    if(size <= DSIZE){
+        asize = 2*DSIZE;
+    }
+    else
+    {
+        asize = DSIZE * ((size + (DSIZE) + (DSIZE -1)) / DSIZE);
+    }
+    if((bp = find_fit(asize)) != NULL ){
+        place(bp,asize);
+        return bp;
+    }
+    extendsize = MAX(asize,CHUNKSIZE);
+    if((bp = extend_heap(extendsize/WSIZE)) == NULL)
+       return NULL;
+    place(bp,asize);
+    return bp;
+
 } 
 
 //
@@ -204,6 +276,22 @@ void *mm_malloc(uint32_t size)
 //
 static void place(void *bp, uint32_t asize)
 {
+    size_t csize = GET_SIZE(HRDP(bp));
+    
+    if((csize - asize) >= (2*DSIZE)) {
+        PUT(HRDP(bp),PACK(asize,1));
+        PUT(FTRP(bp),PACK(asize,1));
+        bp = NEXT_BKLP(bp);
+        PUT(HRDP(bp),PACK(csize - asize,0));
+        PUT(FTRP(bp), PACK(csize - asize,0));
+        
+        
+    }
+    else
+    {
+        PUT(HRDP(bp),PACK(csize ,1));
+        PUT(FTRP(bp), PACK(csize,1));
+    }
 }
 
 
@@ -295,4 +383,3 @@ static void checkblock(void *bp)
     printf("Error: header does not match footer\n");
   }
 }
-
